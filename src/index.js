99bfdfacdbee32b779e41fce07aa7ae6d42c1584
YoungRong.Oh <youngrong.oh@gmail.com>
2021-04-03 23:51:07 +0900

    Store & Reducer

--- src/index.js
+++ src/index.js
@@ -1,24 +1,10 @@
+import { createStore } from 'redux';
 const add = document.querySelector('#add');
 const minus = document.querySelector('#minus');
 const number = document.querySelector('#number');
 
-let count = 0;
-
-number.innerText = count;
-
-const updateText = () => {
-  number.innerText = count;
+const countModifier = (count = 0, action) => {
+  return count;
 };
 
-const handleAdd = () => {
-  count = count + 1;
-  updateText();
-};
-
-const handleMinus = () => {
-  count = count - 1;
-  updateText();
-};
-
-add.addEventListener('click', handleAdd);
-minus.addEventListener('click', handleMinus);
+const countStore = createStore(countModifier);
