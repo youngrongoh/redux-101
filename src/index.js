e0778c624406d9e5dc4dde47ac7e91903e9b18c8
YoungRong.Oh <youngrong.oh@gmail.com>
2021-04-03 23:51:24 +0900

    Action & Dispatch

--- src/index.js
+++ src/index.js
@@ -4,7 +4,22 @@ const minus = document.querySelector('#minus');
 const number = document.querySelector('#number');
 
 const countModifier = (count = 0, action) => {
-  return count;
+  if (action.type === 'ADD') {
+    return count + 1;
+  } else if (action.type === 'MINUS') {
+    return count - 1;
+  } else {
+    return count;
+  }
 };
 
 const countStore = createStore(countModifier);
+
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'MINUS' });
+
+console.log(countStore.getState());
