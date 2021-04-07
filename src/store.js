994447e95f36b3aa381ffc0ebf271aee736d9616
YoungRong.Oh <youngrong.oh@gmail.com>
2021-04-07 18:12:49 +0900

    createReducer 도입

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import { createStore } from 'redux';
-import { createAction } from '@reduxjs/toolkit';
+import { createAction, createReducer } from '@reduxjs/toolkit';
 import LocalStore from './localstore';
 
 const localStore = new LocalStore();
@@ -8,22 +8,18 @@ const addToDo = createAction('ADD_TODO');
 const deleteToDo = createAction('DELETE_TODO');
 const loadFromLocal = createAction('LOAD_FROM_LOCAL');
 
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    case addToDo.type:
-      const updated = [{ text: action.payload, id: Date.now() }, ...state];
-      localStore.saveToLocal('toDos', updated);
-      return updated;
-    case deleteToDo.type:
-      const filtered = state.filter(({ id }) => id !== action.payload);
-      localStore.saveToLocal('toDos', filtered);
-      return filtered;
-    case loadFromLocal.type:
-      return localStore.loadFromLocal('toDos');
-    default:
-      return state;
-  }
-};
+const reducer = createReducer([], {
+  [addToDo]: (state, action) => {
+    state.push({ text: action.payload, id: Date.now() });
+    localStore.saveToLocal('toDos', state);
+  },
+  [deleteToDo]: (state, action) => {
+    const filtered = state.filter(({ id }) => id !== action.payload);
+    localStore.saveToLocal('toDos', filtered);
+    return filtered;
+  },
+  [loadFromLocal]: () => localStore.loadFromLocal('toDos'),
+});
 
 const store = createStore(reducer);
 
