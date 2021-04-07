b126c3477641f6d222fe169b29886845c2f98c7a
YoungRong.Oh <youngrong.oh@gmail.com>
2021-04-07 17:57:33 +0900

    createAction 도입

--- src/store.js
+++ src/store.js
@@ -1,27 +1,24 @@
 import { createStore } from 'redux';
+import { createAction } from '@reduxjs/toolkit';
 import LocalStore from './localstore';
 
 const localStore = new LocalStore();
 
-const ADD_TODO = 'ADD_TODO';
-const DELETE_TODO = 'DELETE_TODO';
-const LOAD_FROM_LOCAL = 'LOAD_FROM_LOCAL';
-
-const addToDo = (text) => ({ type: ADD_TODO, text });
-const deleteToDo = (id) => ({ type: DELETE_TODO, id: +id });
-const loadFromLocal = () => ({ type: LOAD_FROM_LOCAL });
+const addToDo = createAction('ADD_TODO');
+const deleteToDo = createAction('DELETE_TODO');
+const loadFromLocal = createAction('LOAD_FROM_LOCAL');
 
 const reducer = (state = [], action) => {
   switch (action.type) {
-    case ADD_TODO:
-      const updated = [{ text: action.text, id: Date.now() }, ...state];
+    case addToDo.type:
+      const updated = [{ text: action.payload, id: Date.now() }, ...state];
       localStore.saveToLocal('toDos', updated);
       return updated;
-    case DELETE_TODO:
-      const filtered = state.filter(({ id }) => id !== action.id);
+    case deleteToDo.type:
+      const filtered = state.filter(({ id }) => id !== action.payload);
       localStore.saveToLocal('toDos', filtered);
       return filtered;
-    case LOAD_FROM_LOCAL:
+    case loadFromLocal.type:
       return localStore.loadFromLocal('toDos');
     default:
       return state;
